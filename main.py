4d9481b5de4a4d50d8e013207b688aa1d5415c35
hineugene <03ugene@gmail.com>
2025-05-01 23:30:07 +0900

    Update main.py

--- main.py
+++ main.py
@@ -12,4 +12,4 @@ if __name__ == "__main__":
 
     model = DFNet.build(input_shape=(10000, 1), classes=MAX_LABEL)
     OPTIMIZER = Adam(learning_rate=0.0005, beta_1=0.9, beta_2=0.999, epsilon=1e-08, decay=0.0)
-    train_loop(model, OPTIMIZER, MAX_LABEL, data, test_size=0.2, first_task = 49, inc_task = 45, first_epochs = 80, inc_epochs = 20, lamb=0, num_sample=100)
+    train_loop(model, OPTIMIZER, MAX_LABEL, data, test_size=0.2, first_task = 49, inc_task = 45, first_epochs = 70, inc_epochs = 20, lamb=0, num_sample=100)
